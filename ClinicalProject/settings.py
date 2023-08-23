988bb8504a4aed8117c5042a94fe8fd837be1337
fateh1990 <54468349+fateh1990@users.noreply.github.com>
2023-08-23 10:36:19 +0530

    Update settings.py

--- ClinicalProject/settings.py
+++ ClinicalProject/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'ClinicalProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clinicalsdb',
+        'USER':'root',
+        'PASSWORD':"test1234'
     }
 }
 
