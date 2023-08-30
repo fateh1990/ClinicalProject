74ee3c78e50ef622caee0132dc97c4b384e457e6
fateh1990 <54468349+fateh1990@users.noreply.github.com>
2023-08-30 08:04:52 +0530

    Update settings.py

--- ClinicalProject/settings.py
+++ ClinicalProject/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'ClinicalProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clinicalsdb',
-        'USER':'root',
-        'PASSWORD':"test1234'
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
